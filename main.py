9e615494f3fb8e136c1cdf09116eed073d68c8a0
henriits <121551949+henriits@users.noreply.github.com>
2023-01-31 18:11:41 +0200

    Adding lines, finishing project

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 20, 200, 20)
+
+    # Creating lines on whole page
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
 
     # Set the footer.
     pdf.ln(265)
@@ -28,6 +31,10 @@ for index, row in df.iterrows():
         pdf.ln(277)
         pdf.set_font(family="Times", style="I", size=10)
         pdf.set_text_color(180, 180, 180)
+
+        # Creating lines on whole page
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
 pdf.output("output.pdf")
